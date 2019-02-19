ccc1ebf54d45c2ef80f5d48c7add4fead8701ebe
GuillaumeLog <47252069+GuillaumeLog@users.noreply.github.com>
2019-02-19 15:56:22 +0100

    Cleaning of previous project/app names

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""personal_website_backend URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
